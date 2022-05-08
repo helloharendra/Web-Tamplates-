1c0c7c119e2be22af19ac559352cbe79c45420fe
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-05-08 12:58:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function hireme_btn(){
     alert(" You are Hired");
 }
+
+function hireme_btn1(){
+    alert("this is portfolio section");
+}
+
+function login_btn(){
+alert("login done");
+}
+function signin_btn(){
+    alert("Successfully sign in");
+    }
